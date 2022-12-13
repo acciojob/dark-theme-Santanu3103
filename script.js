6fe48a2257d1b154c05f145ea9d4ab06bdcb8acb
santunu <santanuroy253396@gmail.com>
2022-12-13 21:33:59 +0530

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-//your code here
+function swap() {
+   let main = document.getElementById("app");
+   main.classList.toggle('night');
+   document.getElementById("swap").classList.toggle('button_night');
+}
 
